9736f361f0728adc31f5288bb68c6d361adeddd9
merma <merma@merma-mint-kde-MRE>
2016-01-06 16:50:51 -0430

    Inicializando el Repositorio

---
+++ js/contact_me.js
@@ -0,0 +1,72 @@
+$(function() {
+
+    $("input,textarea,select").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var nombre 		= $("input#nombre").val();
+            var email 		= $("input#email").val();
+            var telefono 	= $("input#telefono").val();
+			var informacion = $("input#informacion").val();
+            var mensaje 	= $("textarea#mensaje").val();
+            var firstName = nombre; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "././mail/contact_me.php",
+                type: "POST",
+                data: {
+                    nombre: nombre,
+                    telefono: telefono,
+                    email: email,
+					informacion: informacion,
+                    mensaje: mensaje
+                },
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Tu información ha sido enviada. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Lo sentimos " + firstName + ", al parecer, el servidor no esta respondiendo. Por favor intenta nuevamnete");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#nombre').focus(function() {
+    $('#success').html('');
+});
